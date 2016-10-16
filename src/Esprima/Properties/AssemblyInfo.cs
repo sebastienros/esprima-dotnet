304c7d59b38a198275eef751f6fb93f6293fafbb
Sebastien Ros <sebastienros@gmail.com>
2016-10-16 11:54:37 -0700

    Updating package information

--- src/Esprima/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Resources;
-
-[assembly: NeutralResourcesLanguage("en-us")]
-[assembly: AssemblyCopyright("© Sebastien Ros. All rights reserved.")]
-[assembly: AssemblyProduct("Esprima .NET")]
