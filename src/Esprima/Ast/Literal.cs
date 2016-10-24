7104df53396df1c2f918701b652087b2882b9c5d
Sebastien Ros <sebastienros@gmail.com>
2016-10-23 17:27:50 -0700

    Support for non-string property keys

--- src/Esprima/Ast/Literal.cs
+++ src/Esprima/Ast/Literal.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 
@@ -90,7 +91,7 @@ namespace Esprima.Ast
 
         string PropertyKey.GetKey()
         {
-            return StringValue;
+            return Convert.ToString(Value);
         }
     }
 }
