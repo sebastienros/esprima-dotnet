5734723cf86d97be5423c98abae54e107d4601cc
Atif Aziz <code@raboof.com>
2019-01-16 19:59:30 +0100

    Move Context as a private detail of the parser (#82)

--- src/Esprima/JavascriptParser.cs
+++ src/Esprima/JavascriptParser.cs
@@ -14,6 +14,22 @@ namespace Esprima
             "<<=", ">>=", ">>>=", "&=", "^=", "|="
         };
 
+        private sealed class Context
+        {
+            public int LastCommentStart;
+            public bool AllowIn;
+            public bool AllowYield;
+            public Token FirstCoverInitializedNameError;
+            public bool IsAssignmentTarget;
+            public bool IsBindingElement;
+            public bool InFunctionBody;
+            public bool InIteration;
+            public bool InSwitch;
+            public bool Strict;
+            public HashSet<string> LabelSet;
+            public Stack<int> MarkerStack;
+        }
+
         private readonly Stack<HoistingScope> _hoistingScopes = new Stack<HoistingScope>();
 
         private Token _lookahead;
