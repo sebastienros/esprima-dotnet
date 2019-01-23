be736dac0a4550c7e95dc25c3622bff29b86df90
Atif Aziz <code@raboof.com>
2019-01-23 09:35:18 +0100

    ParserException API clean-up

--- src/Esprima/ErrorHandler.cs
+++ src/Esprima/ErrorHandler.cs
@@ -34,16 +34,7 @@ namespace Esprima
 
         public ParserException CreateError(int index, int line, int col, string description)
         {
-            var msg = $"Line {line}: {description}";
-            var error = new ParserException(msg)
-            {
-                Index = index,
-                Column = col,
-                LineNumber = line,
-                Description = description,
-                SourceText = Source
-            };
-            return error;
+            return new ParserException(description, Source, index, line, col);
         }
 
         public void TolerateError(int index, int line, int col, string description)
--- src/Esprima/ParserException.cs
+++ src/Esprima/ParserException.cs
@@ -4,14 +4,41 @@ namespace Esprima
 {
     public class ParserException : Exception
     {
-        public int Column;
-        public string Description;
-        public int Index;
-        public int LineNumber;
-        public string SourceText;
+        public string Description { get; }
+        public string SourceText  { get; }
+        public int Index          { get; }
+        public int LineNumber     { get; }
+        public int Column         { get; }
 
-        public ParserException(string message) : base(message)
+        public ParserException() :
+            this(null) {}
+
+        public ParserException(string message) :
+            this(message, null) {}
+
+        public ParserException(string description,
+            string sourceText, int index, int lineNumber, int column) :
+            this(null, description, sourceText, index, lineNumber, column) {}
+
+        public ParserException(string message, string description) :
+            this(message, description, null, 0, 0, 0) {}
+
+        public ParserException(string message, string description,
+            string sourceText, int index, int lineNumber, int column) :
+            base(message ?? FormatDefaultMessage(description, lineNumber))
         {
+            Description = description;
+            SourceText  = sourceText;
+            Index       = index;
+            LineNumber  = lineNumber;
+            Column      = column;
         }
+
+        static string FormatDefaultMessage(string description, int lineNumber)
+            => description is string desc
+             ? lineNumber > 0
+             ? $"Line {lineNumber}: {desc}"
+             : desc
+             : null;
     }
 }
