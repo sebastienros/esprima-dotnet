af31f0a19c82890533325bc4004fe1bf1750e0b3
Sam Lord <sam.lord@squaredup.com>
2019-02-13 09:40:35 +0000

    Tidy up arrow functions
    Also allow start and end position to be the same on lines other than 0.
    IFunction now uses INode meaning StatementBlock | Expression, so
    ArrowFunctionExpression can implement IFunction.

--- src/Esprima/Ast/ArrowFunctionExpression.cs
+++ src/Esprima/Ast/ArrowFunctionExpression.cs
@@ -2,15 +2,21 @@
 
 namespace Esprima.Ast
 {
-    public class ArrowFunctionExpression : Node, Expression
+    public class ArrowFunctionExpression : Node, Expression, IFunction
     {
-        public readonly Identifier Id;
-        public readonly List<INode> Params;
-        public readonly INode Body; // : BlockStatement | Expression;
-        public readonly bool Generator;
-        public readonly bool Expression;
+        public bool Strict { get; }
 
-        public HoistingScope HoistingScope;
+        public Identifier Id { get; }
+
+        public List<INode> Params { get; }
+
+        public INode Body { get; } // : BlockStatement | Expression;
+
+        public bool Generator { get; }
+
+        public bool Expression { get; }
+
+        public HoistingScope HoistingScope { get; }
 
         public ArrowFunctionExpression(List<INode> parameters, INode body, bool expression, HoistingScope hoistingScope) :
             base(Nodes.ArrowFunctionExpression)
