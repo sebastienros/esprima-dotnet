112aba78783218565c9c28712f918f15870ddf7a
Marko Lahma <marko.lahma@gmail.com>
2020-02-29 09:23:09 +0200

    Async constructor error message should follow V8 - https://github.com/jquery/esprima/commit/72197311262ea6daf1a642535025cf8f0bc0c06f

--- src/Esprima/Messages.cs
+++ src/Esprima/Messages.cs
@@ -57,5 +57,6 @@
         public const string ForInOfLoopInitializer = "'{0} loop variable declaration may not have an initializer";
         public const string DeclarationMissingInitializer = "Missing initializer in {0} declaration";
         public const string LetInLexicalBinding = "let is disallowed as a lexically bound name";
+        public const string ConstructorIsAsync = "Class constructor may not be an async method";
     };
 }
