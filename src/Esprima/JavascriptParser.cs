cfe9ff09db8f6e8f2fc0a90e0ab0e9f09617233a
tschombe <tschombe76@web.de>
2020-10-21 20:42:27 +0200

    Make tokens public available in JavaScriptParser (#130)

--- src/Esprima/JavascriptParser.cs
+++ src/Esprima/JavascriptParser.cs
@@ -58,7 +58,8 @@ namespace Esprima
         private bool _hasLineTerminator;
         private readonly Action<Node>? _action;
 
-        internal List<Token> Tokens = new List<Token>();
+        private List<Token> _tokens = new List<Token>();
+        public IReadOnlyList<Token> Tokens => _tokens;
 
         // cache frequently called funcs so we don't need to build Func<T> intances all the time
         private readonly Func<Expression> parseAssignmentExpression;
@@ -291,7 +292,7 @@ namespace Esprima
 
             if (_config.Tokens && next != null && next.Type != TokenType.EOF)
             {
-                Tokens.Add(ConvertToken(next));
+                _tokens.Add(ConvertToken(next));
             }
 
             return token!;
@@ -307,9 +308,9 @@ namespace Esprima
             {
                 // Pop the previous token, '/' or '/='
                 // This is added from the lookahead token.
-                Tokens.RemoveAt(Tokens.Count - 1);
+                _tokens.RemoveAt(_tokens.Count - 1);
 
-                Tokens.Add(ConvertToken(token));
+                _tokens.Add(ConvertToken(token));
             }
 
             // Prime the next lookahead.
