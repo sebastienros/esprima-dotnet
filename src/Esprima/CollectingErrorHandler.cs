7542db9cdc325492c30bf013918318add91a0d07
Marko Lahma <marko.lahma@gmail.com>
2021-01-26 20:13:38 +0200

    Separate collecting logic for error handler

---
+++ src/Esprima/CollectingErrorHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Esprima
+{
+    /// <summary>
+    /// Error handler that collects errors that have been seen during the parsing.
+    /// </summary>
+    /// <remarks>
+    /// If you reuse this instance memory usage can grow during process lifetime when errors
+    /// are gathered.
+    /// </remarks>
+    public sealed class CollectingErrorHandler : ErrorHandler
+    {
+        private readonly List<ParserException> _errors = new List<ParserException>();
+
+        public IReadOnlyCollection<ParserException> Errors => _errors;
+
+        public override void RecordError(ParserException error)
+        {
+            _errors.Add(error);
+        }
+    }
+}
