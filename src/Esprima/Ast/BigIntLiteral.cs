0470950bf91c1b15b39d0cb9be365624f99d046a
Jochen Kühner <jochen.kuehner@gmx.de>
2021-11-01 19:55:30 +0100

    bigint & numeric seperator support

---
+++ src/Esprima/Ast/BigIntLiteral.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+namespace Esprima.Ast
+{
+    public sealed class BigIntLiteral : Literal
+    {
+        public readonly string BigInt;
+
+        public BigInteger? BigIntValue => (BigInteger?) Value;
+
+        public BigIntLiteral(BigInteger value, string raw) : base(TokenType.BigIntLiteral, value, raw)
+        {
+            BigInt = raw;
+        }
+    }
+}
