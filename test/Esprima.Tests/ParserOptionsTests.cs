2f5c247bfd3ead0630f33f1960520e86a82a1c7c
Adam Simon <adamosimoni@gmail.com>
2024-02-19 10:35:38 +0100

    Fix copy ctor of ParserOptions (default implementation doesn't deep clone record type fields)

---
+++ test/Esprima.Tests/ParserOptionsTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Esprima.Tests;
+
+public class ParserOptionsTests
+{
+    [Fact]
+    public void CopyCtorShouldCreateDeepClone()
+    {
+        var options1 = new ParserOptions { Tolerant = false };
+        var options2 = options1 with { Tolerant = true };
+
+        Assert.NotSame(options1.GetScannerOptions(), options2.GetScannerOptions());
+        Assert.True(options2.Tolerant);
+        Assert.False(options1.Tolerant);
+    }
+
+    [Fact]
+    public void EqualsShouldCheckStructuralEquality()
+    {
+        var options1 = new ParserOptions { Tolerant = false };
+        var options2 = options1 with { Tolerant = false };
+
+        Assert.NotSame(options1.GetScannerOptions(), options2.GetScannerOptions());
+        Assert.Equal(options1.GetScannerOptions(), options2.GetScannerOptions());
+        Assert.Equal(options1, options2);
+    }
+}
